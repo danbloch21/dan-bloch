0084288b565210b093fce48901dc0a8ceed35611
Dan Bloch <danjobloch@gmail.com>
2021-06-18 10:05:47 +0100

    updated react personal site

--- src/components/MiddleContainer.js
+++ src/components/MiddleContainer.js
@@ -21,14 +21,13 @@ function MiddleContainer() {
             <h3>Eagle eyes</h3>
             <p>With top-notch editing skills honed from a 20-year-plus career in business magazine publishing, I can spot a typo from a mile off and polish your content so it makes its mark</p>
             <h3>Examples of work</h3>
-            <p><a className="footer-link" href="https://fem-challenge-1-lilac.vercel.app/">Profile card</a>
-            </p>
-            <p><a className="footer-link" href="https://responsive-design-tindog-coral.vercel.app/">Tindog</a></p><a className="footer-link" href="https://responsive-design-tindog-coral.vercel.app/">
-            </a><p><a className="footer-link" href="https://responsive-design-tindog-coral.vercel.app/" /><a className="footer-link" href="https://fem-challenge-2-one.vercel.app/">Four card feature design</a></p><a className="footer-link" href="https://fem-challenge-2-one.vercel.app/">
-            </a><p><a className="footer-link" href="https://fem-challenge-2-one.vercel.app/" /><a className="footer-link" href="https://react-app-to-do.vercel.app/">React to do list</a>
-            </p><p><a className="footer-link" href="https://metronomeo-react.vercel.app/">React metronome</a>
-            </p><p><a className="footer-link" href="https://books-library-iota.vercel.app">React books library</a>
-            </p></div>
+            <p><a className="footer-link" href="https://fem-challenge-1-lilac.vercel.app/">Profile card</a></p>
+            <p><a className="footer-link" href="https://responsive-design-tindog-coral.vercel.app/">Tindog</a></p>
+            <p><a className="footer-link" href="https://fem-challenge-2-one.vercel.app/">Four card feature design</a></p>
+            <p><a className="footer-link" href="https://react-app-to-do.vercel.app/">React to do list</a></p>
+            <p><a className="footer-link" href="https://metronomeo-react.vercel.app/">React metronome</a>
+            </p><p><a className="footer-link" href="https://books-library-iota.vercel.app">React books library</a></p>
+            </div>
         </div>
       </div>
 
